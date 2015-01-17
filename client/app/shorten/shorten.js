800c5c26f46685c10e4fc5f90998522086c14156
tmartin1 <timothy.m.martin@gmail.com>
2015-01-17 12:06:14 -0800

    Added search and filter to links.

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,14 +1,30 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
+
   angular.extend($scope, Links);
+
   $scope.link = "";
+
   $scope.shorten = function(link) {
     validate(link) ? $scope.addLink({url:link}) : console.log("What? That's not a proper url!");
   };
+
   function validate(link) {
     if (link.slice(0,7) !== "http://") return false;
     rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
     return !!link.match(rValidUrl);
+  };
+
+  // function submit() {
+
+  // };
+
+  $scope.isEnter = function($scope, key) {
+    console.log(event);
+    $scope.keydown = function() {
+      //
+    };
+    return event.key === key;
   };
 });
